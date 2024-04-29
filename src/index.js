2076d7110e7486af3efd25b81adf2a4f8816c011
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-29 21:18:06 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDom from "react-dom";
+ReactDom.render(<p>hello</p>, document.getElementById("root"));
+ReactDom.render(
+  <div>
+    <p>hello</p>,<h1>welcome</h1>
+  </div>,
+  document.getElementById("root")
+);
