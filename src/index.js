f9a5dd51b46836c91f5585b160b9355d449853b3
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-30 13:10:17 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
-ReactDom.render(<p>hello</p>, document.getElementById("root"));
+// ReactDom.render(<p>hHello</p>, document.getElementById("root"));
 ReactDom.render(
   <div>
     <p>hello</p>,<h1>welcome</h1>
